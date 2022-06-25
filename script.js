f4eb7594924e4b978c1fa790806535f768ce09a4
Valney Rodrigues <97922437+Valneyxis@users.noreply.github.com>
2022-06-25 09:41:29 -0300

    inicio crud

---
+++ script.js
@@ -0,0 +1,10 @@
+const modal = document.querySelector('.modal-container')
+const tbody = document.querySelector('tbody')
+const sNome = document.querySelector('#m-nome')
+const sTelefone = document.querySelector('#m-telefone')
+const sEmail = document.querySelector('#m-email')
+const btnSalvar = document.querySelector('#btnSalvar')
+
+let itens
+let id
+
